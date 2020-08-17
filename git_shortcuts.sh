f6e8f664c49c48a61c93e7e3563ebcd5bd55bc66
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-17 16:53:34 +0200

    Update #4 -- Updated gitpom function

--- git_shortcuts.sh
+++ git_shortcuts.sh
@@ -1,27 +1,27 @@
 # Git add
 function gita {
-
+  cd
 }
 
 # Git commit
-function gitc {
-
+function gitci {
+  cd
 }
 # Git push origin name_of_branch
 function gitpo {
-
+  cd
 }
 
 function gitpom {
-  
+  git push origin master
 }
 
 # Git checkout name_of_branch
 function gitco {
-
+  cd
 }
 
 # Git branch
 function gitbr {
-
+  cd
 }

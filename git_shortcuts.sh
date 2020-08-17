f74258398963d87feba92919e6e8ab7577eb6e9c
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-17 16:49:20 +0200

    Update #2 -- Gave name to bash functions

--- git_shortcuts.sh
+++ git_shortcuts.sh
@@ -1,9 +1,23 @@
 # Git add
+function gita {
+
+}
 
 # Git commit
+function gitc {
 
+}
 # Git push origin name_of_branch
+function gitpo {
+
+}
 
 # Git checkout name_of_branch
+function gitco {
+
+}
 
 # Git branch
+function gitbr {
+
+}

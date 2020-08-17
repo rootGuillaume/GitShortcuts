ee715decdc6bd976592ed940aa7fa4c62d1431c1
rootGuillaume <guillaumegasmi@protonmail.com>
2020-08-17 17:18:08 +0200

    Update 11 -- Created gitciu & gitcib bash functions

--- git_shortcuts.sh
+++ git_shortcuts.sh
@@ -8,6 +8,14 @@ function gitci {
   git commit -m $1
 }
 
+function gitciu {
+  git commit -m "Update $1 -- $2"
+}
+
+function gitcib {
+  git commit -m "Build $1 -- $2"
+}
+
 # Git push origin name_of_branch
 function gitpo {
   git push origin $1
